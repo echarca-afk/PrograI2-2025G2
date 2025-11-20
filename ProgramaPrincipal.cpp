549c3f6fab892474bf7e87f198e049639cb30da2
echarca-afk <echarca@ucb.edu.bo>
2025-11-20 17:41:28 -0400

    Add files via upload

---
+++ ProgramaPrincipal.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "libEmiliano.h"
+using namespace std;
+
+int main(){
+    cout << "El resultado de la multiplicacion es:" << multiplicar(4,5);
+    return 0;
+}
---
+++ libEmiliano.h
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int multiplicar(int valor1, int valor2){
+    return valor1*valor2;
+}
