f528d3e9f0ba6de71a61677a1d00af7588a1c450
echarca-afk <echarca@ucb.edu.bo>
2025-11-20 18:57:06 -0400

    Add files via upload

--- ProgramaPrincipal.cpp
+++ ProgramaPrincipal.cpp
@@ -1,50 +1,62 @@
 #include <iostream>
 #include "libEmiliano.h"
 using namespace std;
-struct Duenio {
-    int idDuenio;      // ID único del dueño
+struct Duenio{
+    int idDuenio;  
     char nombre[30];
     char apellido[30];
     char direccion[50];
     char telefono[15];
     char email[30];
 };
-struct Mascota {
+struct Mascota{
     int idMascota;    // ID único de la mascota
-    int idDuenio;     // Relación con dueño
+    int idDuenio;
     char nombre[30];
-    char especie[20]; // Ej: perro, gato
+    char especie[20]; 
     char raza[30];
-    char sexo[10];    // Ej: macho, hembra
-    int edad;         // En años
+    char sexo[10]; 
+    int edad;      
 };
+
 struct consulta{
+    int idConsulta;
+    int idMascota;
+    char fecha_consulta[11];
     char motivo[100];
 };
-struct HistorialClinico {
-    int idHistorial;  // ID único del historial
-    int idMascota;    // Relación con mascota
-    char fecha[11];   // Formato: DD/MM/AAAA
-    char motivoConsulta[50];
-    char diagnostico[100];
-    char tratamiento[100];
-    char veterinario[30];
-};
+
 struct Tratamiento {
     int idTratamiento;
     char nombre[30];
     char descripcion[100];
     float costo;
 };
-struct RegistroVeterinaria {
-    Mascota mascota;
-    HistorialClinico historial;
+
+struct HistorialClinico{
+    int idHistorial; 
+    int idMascota;  
+    int idConsulta;
+    char fecha[11];   // Formato: DD/MM/AAAA
+    char diagnostico[100];
+    char veterinario[30];
+    int idTratamiento;
 };
-/*Dueno = datos de la persona responsable.
-Mascota = los datos básicos del animal.
-Consulta = cada visita/chequeo.
-Tratamiento = medicinas o procesos que recibe.
-HistorialClinico = junta consultas + tratamientos.
+
+struct facturacion{
+    int idTratamiento;
+    float costo;
+    int nit;
+    char beneficiario[100];
+    char fecha_facturacion[11];
+};
+
+
+/*Dueno = datos de la persona responsable
+Mascota = los datos básicos del animal
+Consulta = cada visita/chequeo
+Tratamiento = medicinas o procesos que recibe
+HistorialClinico = junta consultas + tratamientos
 RegistroVeterinaria = une mascota + historial en un solo registro*/
 
 int main(){
