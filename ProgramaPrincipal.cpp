415b9315f643f6f238115e9393bb1a2391fab31b
echarca-afk <echarca@ucb.edu.bo>
2025-11-20 17:43:53 -0400

    Update ProgramaPrincipal.cpp

--- ProgramaPrincipal.cpp
+++ ProgramaPrincipal.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main(){
+    cout << "El resultado de la suma es:" << sumar(4,5);
     cout << "El resultado de la multiplicacion es:" << multiplicar(4,5);
+    cout << "El resultado de la divison es:" << Division(4,5);
     return 0;
 }
+
