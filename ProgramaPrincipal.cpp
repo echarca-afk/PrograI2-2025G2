c0794da2976695e3f50222d475bb77895c403b1c
echarca-afk <echarca@ucb.edu.bo>
2025-11-25 16:10:24 -0400

    Update ProgramaPrincipal.cpp

--- ProgramaPrincipal.cpp
+++ ProgramaPrincipal.cpp
@@ -1,13 +1,64 @@
-#include<iostream>
-#include"libNoel.h"
+#include <bits/stdc++.h>
+#include "libEmiliano.h"
 using namespace std;
-int main()
-{
-    cout << "el resultado es " << sumar(5,4);
+struct Duenio{
+    int idDuenio;  
+    char nombre[30];
+    char apellido[30];
+    char direccion[50];
+    char telefono[15];
+    char email[30];
+};
+struct Mascota{
+    int idMascota;    // ID único de la mascota
+    int idDuenio;
+    char nombre[30];
+    char especie[20]; 
+    char raza[30];
+    char sexo[10]; 
+    int edad;      
+};
 
-    
+struct consulta{
+    int idConsulta;
+    int idMascota;
+    char fecha_consulta[11];
+    char motivo[100];
+};
+
+struct Tratamiento {
+    int idTratamiento;
+    char nombre[30];
+    char descripcion[100];
+    float costo;
+};
+
+struct HistorialClinico{
+    int idHistorial; 
+    int idMascota;  
+    int idConsulta;
+    char fecha[11];   // Formato: DD/MM/AAAA
+    char diagnostico[100];
+    char veterinario[30];
+    int idTratamiento;
+};
+
+struct facturacion{
+    int idTratamiento;
+    float costo;
+    int nit;
+    char beneficiario[100];
+    char fecha_facturacion[11];
+};
+/*Dueno = datos de la persona responsable
+Mascota = los datos básicos del animal
+Consulta = cada visita/chequeo
+Tratamiento = medicinas o procesos que recibe
+HistorialClinico = junta consultas + tratamientos
+RegistroVeterinaria = une mascota + historial en un solo registro*/
+int main(){
+    string RegistroTXT = "RegistroVeterinaria.txt";
     return 0;
 }
-
 
 
